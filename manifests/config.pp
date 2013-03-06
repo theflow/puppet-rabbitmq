f64de37074b7cff94703d786d80312823a21382c
cory <cory.monty@gmail.com>
2013-03-06 12:57:14 -0600

    adds to config::path

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,6 +3,6 @@ class rabbitmq::config {
 
   $executable = "${boxen::config::home}/homebrew/opt/rabbitmq/sbin/rabbitmq-server"
   $conf_file = "${boxen::config::home}/homebrew/etc/rabbitmq/rabbitmq-env.conf"
-  $path = "${boxen::config::home}/homebrew/bin"
+  $path = "${boxen::config::home}/homebrew/bin:/usr/local/sbin:/usr/bin:/bin:/usr/local/bin"
   $port = 15672
 }
